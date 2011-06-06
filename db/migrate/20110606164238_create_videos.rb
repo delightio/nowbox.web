da935d393810b4b4a62caf27efc0c03d791c3823
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-06 16:48:17 -0700

    Add video model/migration.

---
+++ db/migrate/20110606164238_create_videos.rb
@@ -0,0 +1,17 @@
+class CreateVideos< ActiveRecord::Migration
+  def self.up
+    create_table :videos do |t|
+      t.string :external_id
+      t.string :source
+      t.string :title
+      t.string :description
+      t.boolean :viewable_mobile
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    
+  end
+end
---
+++ models/video.rb
@@ -0,0 +1,13 @@
+module Aji
+  class Video
+    # ## Video Schema
+    # - id: Integer
+    # - external_id: String
+    # - source: String
+    # - title: String
+    # - description: String
+    # - viewable_mobile: Boolean
+    # - created_at: DateTime
+    # - updated_at: DateTime
+  end
+end
