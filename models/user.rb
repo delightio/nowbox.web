55e5aac170cbd59d5a96ac2c7eece9b7328627fe
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-06 16:40:59 -0700

    Create users model/migration.

---
+++ models/user.rb
@@ -0,0 +1,9 @@
+module Aji
+  class User < ActiveRecord::Base
+    # ## User Schema
+    # - id: Integer
+    # - email: String
+    # - created_at: DateTime
+    # - updated_at: DateTime
+  end
+end
