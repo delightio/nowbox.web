2be2ee5fe9cfde5e7b2c5cc808ad4b5cc1ee37a5
Thomas Pun <tpun@ottomator.com>
2011-06-07 16:12:07 -0700

    place holders for channel and user objects

---
+++ controllers/users_controller.rb
@@ -0,0 +1,20 @@
+module Aji
+  class API
+    version '1'
+    
+    resource :users do
+      
+      get '/:user_id' do
+        user = User.find params[:user_id] or
+          no_user_error params[:user_id]
+      end
+      
+      helpers do
+        def no_user_error id
+          error! "User[#{id}] does not exist", 404
+        end
+      end
+      
+    end
+  end
+end
