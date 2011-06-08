c1096aee71e43e810d0f303620fcb7d12521b421
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-08 09:33:26 -0700

    Updated inline video schema.

--- models/video.rb
+++ models/video.rb
@@ -1,6 +1,7 @@
 module Aji
   # ## Video Schema
   # - id: Integer
+  # - author_id: Integer (Foreign Key)
   # - external_id: String
   # - source: String
   # - title: String
@@ -10,8 +11,11 @@ module Aji
   # - updated_at: DateTime
   class Video < ActiveRecord::Base
     has_many :events
+    belongs_to :author
+
     def self.find_or_create_from_hash h
-      Aji::Video.find_or_create_by_external_id h[:external_id] # TODO: need to check all fields
+      # TODO: need to check all fields
+      Aji::Video.find_or_create_by_external_id h[:external_id]
     end
   end
 end
