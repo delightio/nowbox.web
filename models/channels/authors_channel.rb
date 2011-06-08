f86cd02d79c6b9dbc5652a74b9cc9c4a9d39974a
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-07 22:42:01 -0700

    Added preliminary yooutube_it stuff.

--- models/channels/authors_channel.rb
+++ models/channels/authors_channel.rb
@@ -2,8 +2,23 @@ module Aji
   module Channels
     class AuthorsChannel < Channel
       has_and_belongs_to_many :authors
+
       def populate
         authors.each_with_index do |a, i|
+          if a.videos.count == 0 and a.video_source == :youtube
+            yt_videos = YouTubeIt::Client.new.videos_by(
+              :user => "#{a.screen_name}", :max_results => 100,
+              :order_by => 'published').videos
+            yt_videos.each do |v|
+              Video.find_or_create(
+                :title => v.title,
+                :external_id => v.video_id.split(':').last,
+                :description => v.description,
+                :author => a,
+                :source => :youtube,
+                :viewable_mobile => v.noembed)            end
+          end
+
           a.videos.members.each_with_index do |v, k|
             # Until I can write my own Redis-backed ZSet class or come up with
             # a suitable interface to Redis::Objects::SortedSet, this is a 
---
+++ spec/models/channels/authors_channel_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe Aji::Channels::AuthorsChannel do
+  describe "#populate" do
+
+  end
+end
