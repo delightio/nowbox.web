85ad7f437defd704d988820bf3e2c36a96694609
Thomas Pun <tpun@ottomator.com>
2011-06-07 22:48:31 -0700

    fixed spec_helper path

--- spec/models/channels/authors_channel_spec.rb
+++ spec/models/channels/authors_channel_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../../spec_helper", __FILE__)
+require File.expand_path("../../../spec_helper", __FILE__)
 
 describe Aji::Channels::AuthorsChannel do
   describe "#populate" do
