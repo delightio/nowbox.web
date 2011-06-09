942d653a78fdf0fb541a871619f2ed5f4578d5b5
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-08 23:32:17 -0700

    Refactor to use builtin creator.

--- models/channels/trending.rb
+++ models/channels/trending.rb
@@ -8,22 +8,24 @@ module Aji
         url = args[:url] || "nowmov.com"
         path = args[:path] || "/live/videos"
         limit = args[:limit] || 100
-        params = "?target=mobile&limit=#{limit.to_i}" # since it's always going to be iPad
+        # Target is mobile since we'll always be going to the iPad
+        params = "?target=mobile&limit=#{limit.to_i}"
         path = path + params
         response = Net::HTTP.get url, path
-        nowmov_hashes = JSON.parse response
-        nowmov_hashes.each_with_index do |nowmov_hash, index|
-                          #  nowmov                     aji
-          mapping_nw_aji = {:service_name           => :video_source,
-                            :author_username        => :screen_name,
-                            :service_external_id    => :external_id,
-                            :service_name           => :source,
-                            :title                  => :title,
-                            :description            => :description }
-          h={}; mapping_nw_aji.each_pair{ |nw,aji| h[aji] = nowmov_hash[nw] }
-          h[:viewable_mobile] = true # since we asked for mobile target
-          video = Aji::Video.find_or_create_from_hash h # TODO: how do we generate author as well?
-          videos[video.id] = index
+        video_hashes = JSON.parse response
+        video_hashes.each_with_index do |video_hash, index|
+          youtube_account = ExternalAccounts::Youtube.find_or_create_from_uid(
+            video_hash[:author_username], :provider => "youtube")
+
+          video = Aji::Video.find_or_create_from_external_id(
+            video_hash[:service_external_id],
+            :external_account => youtube_account,
+            :source           => video_hash[:service_name],
+            :title            => video_hash[:title],
+            :description      => video_hash[:description],
+            :viewable_mobile  => true) # since we specified mobile target.
+          # Place the video in the channel.
+          content_zset[video.id] = index
         end
       end
 
