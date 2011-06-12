ad2920712368a30591ce52e159d23f33b87d1a0f
Thomas Pun <tpun@ottomator.com>
2011-06-11 19:36:44 -0700

    more comments on hacks needed for heroku deployment [#27]

--- aji.rb
+++ aji.rb
@@ -23,9 +23,8 @@ module Aji
   # Load settings from configs or environment variables.
   # SETTINGS = YAML.load_file("./config/settings.yml")[RACK_ENV]
   
-  # Establish ActiveRecord conneciton and run all necessary migration
-  
-  # HACK
+  # HACK: Heroku reture a ERB version of config/database.yml. 
+  # I had to manually do the following for deployment.
   if ENV['DATABASE_URL']
     uri = URI.parse ENV['DATABASE_URL']
     adapter = uri.scheme
@@ -37,11 +36,11 @@ module Aji
                  :port => uri.port,
                  :username => uri.user,
                  :password => uri.password,
-                 :params => CGI.parse(uri.query || "")
-      }
+                 :params => CGI.parse(uri.query || "") }
   else
     dbconfig = YAML.load_file('config/database.yml')[RACK_ENV]
   end
+  # Establish ActiveRecord conneciton and run all necessary migration
   ActiveRecord::Base.establish_connection dbconfig
   ActiveRecord::Migrator.migrate("db/migrate/")
   
@@ -56,6 +55,10 @@ module Aji
     end
   end
 end
+
+# HACK: % heroku config => shows Ruby 1.9.1
+# The deployed app actually works as expected. However, %heroku run console
+# does not know anything about Aji::* objects.
 
 # Dir.glob("models/*.rb").each { |r| require_relative r }
 # # Must load channel subtypes after other models for dependency reasons.
