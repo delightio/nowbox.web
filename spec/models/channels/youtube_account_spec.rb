03978ec31561b9e5123f15437f651e8a039fcd23
Thomas Pun <tpun@ottomator.com>
2011-06-14 02:32:42 -0700

    update spec test to check for author

--- spec/models/channels/youtube_account_spec.rb
+++ spec/models/channels/youtube_account_spec.rb
@@ -3,16 +3,14 @@ require File.expand_path("../../../spec_helper", __FILE__)
 describe Aji::Channels::YoutubeAccount do
   describe "#populate" do
     it "fetches videos from youtube" do
-      nicole = Aji::ExternalAccounts::Youtube.new :provider => "youtube",
-        :uid => "nicnicolecole"
-      nicole.save
-
-      yc = Aji::Channels::YoutubeAccount.new(:accounts => Array(nicole),
-                                :title => "nicnicolecole's channel")
-      yc.save
-      yc.content_videos.should be_empty
+      youtube_username = "nicnicolecole"
+      nicole = Aji::ExternalAccounts::Youtube.create :provider => "youtube",
+                                                     :uid => youtube_username
+      yc = Aji::Channels::YoutubeAccount.create(:accounts => Array(nicole),
+                                                :title => "#{youtube_username}'s channel")
       yc.populate
-      yc.content_videos.should_not be_empty
+      h = JSON.parse yc.content_videos.first.to_json
+      h["video"]["author"]["username"].should == youtube_username
     end
 
   end
