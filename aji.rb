b2afb59551baa88940a22782112593d1151b62e1
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-20 11:07:29 -0700

    Whitespace fixes.

--- aji.rb
+++ aji.rb
@@ -14,13 +14,13 @@ module Aji
     ENV[key] || YAML.load_file(setting_yml)[RACK_ENV][key]
   end
   BASE_URL = Aji.conf('BASE_URL') || 'localhost'
-  
+
   # Establish Redis connection.
   redis_url = Aji.conf 'REDISTOGO_URL'
   REDIS = Redis.connect :url=>redis_url
   Redis::Objects.redis = REDIS
-  
-  # HACK: Heroku returns a ERB version of config/database.yml. 
+
+  # HACK: Heroku returns a ERB version of config/database.yml.
   # I had to manually do the following for deployment.
   if ENV['DATABASE_URL']
     uri = URI.parse ENV['DATABASE_URL']
@@ -40,7 +40,7 @@ module Aji
   # Establish ActiveRecord conneciton and run all necessary migration
   ActiveRecord::Base.establish_connection dbconfig
   ActiveRecord::Migrator.migrate("db/migrate/")
-  
+
   # An application specific error class.
   class Error < RuntimeError; end
   # An error to raise when a required interface method has not been overridden
@@ -67,7 +67,7 @@ end
 Dir.glob("models/*.rb").each { |r| require "#{Aji.root}/#{r}" }
 # Must load channel subtypes after other models for dependency reasons.
 Dir.glob("models/channels/*.rb").each { |r| require "#{Aji.root}/#{r}" }
-Dir.glob("models/external_accounts/*.rb").each { |r| require  "#{Aji.root}/#{r}" }
+Dir.glob("models/external_accounts/*.rb").each { |r| require "#{Aji.root}/#{r}" }
 Dir.glob("helpers/*.rb").each { |r| require "#{Aji.root}/#{r}" }
 Dir.glob("controllers/*_controller.rb").each { |r| require "#{Aji.root}/#{r}" }
 
