01c02ea85925b2df416eb1ba8eb56e7ced532139
Thomas Pun <tpun@ottomator.com>
2011-06-26 18:39:23 -0700

    sometimes the video returned from nowmov can be invalid

--- spec/models/channels/trending_spec.rb
+++ spec/models/channels/trending_spec.rb
@@ -3,13 +3,13 @@ require File.expand_path("../../../spec_helper", __FILE__)
 describe Aji::Channels::Trending do
   describe "#populate" do
     it "fetches some videos from nowmov.com" do
-      n = 2
+      n = 5
       trending = Aji::Channels::Trending.create :title => "Trending channel from spec test"
       trending.content_videos.should be_empty
       Aji::Video.count.should == 0
       trending.populate :limit=>n
-      Aji::Video.count.should == n
-      trending.content_videos.count.should == n
+      Aji::Video.count.should > 0
+      trending.content_videos.count.should == Aji::Video.count
     end
 
   end
