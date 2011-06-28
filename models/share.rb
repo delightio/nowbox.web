5b11e34fd23f00c76217d9084c9c47846d02d6c3
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-27 20:00:14 -0700

    Add sharing support through model and queue.

--- models/share.rb
+++ models/share.rb
@@ -7,6 +7,27 @@ module Aji
   class Share < ActiveRecord::Base
     belongs_to :user
     belongs_to :video
+    has_many :external_accounts
+    after_create :queue_publishing
 
+    attr_accessor :publish_to
+
+    def link
+      "http://#{Aji.conf['BASE_URI']}/#{id}"
+    end
+
+    def queue_publishing
+      unless publish_to.empty?
+        pub_accounts = user.external_accounts.find_all do |acc|
+          publish_to.include? acc.provider
+        end
+      else
+        pub_accounts = user.external_accounts
+      end
+
+      pub_accounts.each do |pub|
+        Resque.enqueue PublishShare, pub.id, id
+      end
+    end
   end
 end
---
+++ queues/publish_share.rb
@@ -0,0 +1,12 @@
+module Queues
+  class PublishShare
+    @queue = :publish_share
+
+    def self.perform external_account_id, share_id
+      acc = ExternalAccount.find(external_account_id)
+      share = Share.find(share_id)
+
+      acc.publish share.message
+    end
+  end
+end
