16835faa249eb70a768b1eed0e5b6d2604a7e60f
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-06-29 12:08:37 -0700

    This looks a lot like what I used to have.
    Guess I just needed to write it twice and waste two days.

--- controllers/auth_controller.rb
+++ controllers/auth_controller.rb
@@ -1,20 +1,25 @@
 module Aji
   class AuthController < Sinatra::Base
+
+    get '/failure' do
+      env_hash = request.env.dup
+      env_hash.inspect
+    end
+
     get '/:provider/callback' do
       auth_hash = request.env['omniauth.auth']
       auth_hash.inspect
 
-      #case params[:provider]
-      #when 'twitter'
-      #  t = ExternalAccounts::Twitter.find_or_create_by_provider_and_uid(
-      #    auth_hash['provider'], auth_hash['uid'],
-      #    :user_info => auth_hash['user_info'],
-      #    :credentials => auth_hash['credentials'])
-      #    t.serializable_hash
-      #else
-      #  [ "Provider #{params[:provider]} not implemented.",
-      #    { 'Content-Type' => 'application/json' }, 400 ]
-      #end
+      case params['provider']
+      when 'twitter'
+        t = ExternalAccounts::Twitter.find_or_create_by_provider_and_uid(
+          auth_hash['provider'], auth_hash['uid'],
+          :credentials => auth_hash['credentials'],
+          :user_info => auth_hash['user_info'])
+        t.serializable_hash.inspect
+      else
+        "Unsupported provider #{auth_hash['provider']}"
+      end
     end
   end
 end
