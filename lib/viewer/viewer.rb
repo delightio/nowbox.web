11d92befc77eec94a84d5a119c8b76607d02b522
Fahd Butt <fahdoo@nowmov.com>
2011-06-29 17:45:48 -0700

    Web frontend landing and company pages [#38 state:committed] [#70 state:committed]

--- lib/viewer/viewer.rb
+++ lib/viewer/viewer.rb
@@ -1,11 +1,33 @@
 require 'sinatra/base'
 
-
 module Aji
   class Viewer < Sinatra::Base
     # Use Erubis for template generation. Essentially a faster ERB.
     Tilt.register :erb, Tilt[:erubis]
+    # Need to explicitly set the template directory http://stackoverflow.com/questions/3742486/sinatra-cannot-find-views-on-ruby-1-9-2-p0
+		set :views, File.dirname(__FILE__) + "/views"
+		set :public, File.dirname(__FILE__) + "/public"
+		
+		get '/' do 
+			erb :index
+		end
 
+		get '/about' do 
+			erb :about
+		end
+		
+		get '/jobs' do 
+			erb :jobs
+		end
+
+		get '/tour' do 
+			erb :tour
+		end
+
+		get '/download' do 
+			erb :download
+		end
+										
     get '/:share_id' do
       @share = Share.find params[:share_id]
       erb :play_share
