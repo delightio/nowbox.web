4f2a3f4337da06bc9ffd708118e7c48dc162b517
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-06 17:19:48 -0700

    Spell out every damn module.

--- queues/examine_video.rb
+++ queues/examine_video.rb
@@ -3,7 +3,7 @@ module Aji
     class ExamineVideo
       # Specify a class attribute `queue` which resque uses for job control.
       @queue = :examine_video
-      
+
       def self.perform video_id
         # TODO: put it in blacklist right the way
         Aji.redis.sadd Channel.blacklisted_video_ids_key, video_id
--- queues/mention/parse.rb
+++ queues/mention/parse.rb
@@ -1,22 +1,26 @@
 module Aji
-  class Queues::Mention::Parse
+  module Queues
+    module Mention
+      class Parse
 
-    @queue = :mention
+        @queue = :mention
 
-    # Handles incoming parse requests from various social feeds. If the mention
-    # contains a valid http(s) link it will be enqueued for processing. At this
-    # time there exists no efficient way to filter out non-video links as link
-    # expansion will be necessary in many cases (fskcing overzealos t.co use by
-    # Twitter)
-    def self.perform source, data
-      # Parse incoming data from various sources.
-      case source
-      when 'twitter'
-        mention = Parsers::Tweet.parse data
+        # Handles incoming parse requests from various social feeds. If the mention
+        # contains a valid http(s) link it will be enqueued for processing. At this
+        # time there exists no efficient way to filter out non-video links as link
+        # expansion will be necessary in many cases (fskcing overzealos t.co use by
+        # Twitter)
+        def self.perform source, data
+          # Parse incoming data from various sources.
+          case source
+          when 'twitter'
+            mention = Parsers::Tweet.parse data
+          end
+
+          # Enqueue iff the mention has a valid link.
+          Resque.enqueue Queues::Mention::Process mention if mention.has_link?
+        end
       end
-
-      # Enqueue iff the mention has a valid link.
-      Resque.enqueue Queueus::Mention::Process mention if mention.has_link?
     end
   end
 end
