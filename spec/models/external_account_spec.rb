d7afabb890312aaa89f3a0ace00439aed0885418
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-09 13:46:01 -0700

    Add stub spec for external accounts.

---
+++ spec/models/external_account_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+describe Aji::ExternalAccount do
+  describe "#publish" do
+    it "should raise an exception unless implemented" do
+      
+    end
+  end
+end
