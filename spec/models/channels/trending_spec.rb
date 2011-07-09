4bcc422437fff14d066deb78e91539caec275f8d
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-09 13:45:44 -0700

    Channel spec is pending.

--- spec/models/channels/trending_spec.rb
+++ spec/models/channels/trending_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../../spec_helper", __FILE__)
 describe Aji::Channels::Trending do
   describe "#populate" do
     it "fetches some videos from nowmov.com" do
+      pending
       n = 5
       trending = Aji::Channels::Trending.create :title => "Trending channel from spec test"
       trending.content_videos.should be_empty
