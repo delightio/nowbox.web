1434f9f4b412544e55c6b4b92716297fbcfabb88
Thomas Pun <tpun@ottomator.com>
2011-07-15 01:27:19 -0700

    use subject

--- spec/queues/mention/parse_spec.rb
+++ spec/queues/mention/parse_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 module Aji
   describe Queues::Mention::Parse do
+    subject { Queues::Mention::Parse }
+    
     describe "#perform" do
       before(:each) do
         @data = mock "raw data"
@@ -12,13 +14,13 @@ module Aji
       it "enqueues to Queues::Mention::Process if mention has links" do
         @mention.stub(:has_link?).and_return(true)
         Resque.should_receive(:enqueue).with(Queues::Mention::Process, @mention)
-        Queues::Mention::Parse.perform "twitter", @data
+        subject.perform "twitter", @data
       end
       
       it "rejects given mention if there is no link" do
         @mention.stub(:has_link?).and_return(false)
         Resque.should_receive(:enqueue).with(Queues::Mention::Process, @mention).never
-        Queues::Mention::Parse.perform "twitter", @data
+        subject.perform "twitter", @data
       end
     end
   end
