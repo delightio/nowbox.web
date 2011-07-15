d532013e4ee751a7f34fa8e569aaa3f70238e6bd
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-15 14:07:16 -0700

    Tweet parser parses link entities.

--- lib/parsers/tweet.rb
+++ lib/parsers/tweet.rb
@@ -5,12 +5,18 @@ module Aji
     # existing one representing the author of the mention.
     def self.parse json
       tweet_hash = MultiJson.decode json
+      author = ExternalAccounts::Twitter.find_or_create_by_provider_and_uid(
+        'twitter', tweet_hash['user']['id'].to_s)
+      links = tweet_hash['entities']['urls'].map do |url|
+        Link.new(url['expanded_url'] || url['url'])
+      end
+
       Mention.new(
         :external_id => tweet_hash['uid'],
         :body => tweet_hash['text'],
         :unparsed_data => json,
-        :author => ExternalAccounts::Twitter.find_or_create_by_provider_and_uid(
-          'twitter', tweet_hash['user']['id'].to_s))
+        :author => author,
+        :links => links)
     end
   end
 end
