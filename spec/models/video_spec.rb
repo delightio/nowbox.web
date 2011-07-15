8d114c52cd54d423f25aa9915d14faf5b4a19d8d
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-14 19:08:58 -0700

    Completely redefined Video model.
    It's brand new, not quite complete. Will be adding
    more and implementing stuff tonight and tomorrow.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,57 +1,76 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Aji::Video do
-  
-  describe "#thumbnail_uri" do
-    it "should always have a uri if source is youtube" do
-      video = Factory :video, :source=>:youtube
-      video.thumbnail_uri.should include "youtube"
-    end
-  end
-  
-  describe "#populate" do
-    it "should not be populated unless explicitly asked" do
-      video = Factory :video, :source => :youtube, :external_id => 'OzVPDiy4P9I'
-      video.is_populated?.should == false
-      video.title.should be_nil
-      video.populate
-      Aji::Video.find(video.id).is_populated?.should == true
-      Aji::Video.find(video.id).title.should_not be_nil
-    end
-  end
-  
-  describe ".find_or_create_from_youtubeit_video" do
-    it "should mark the video populated" do
-      yt_video = YouTubeIt::Client.new.video_by 'OzVPDiy4P9I'
-      video = Aji::Video.find_or_create_from_youtubeit_video yt_video
-      Aji::Video.find(video.id).is_populated?.should == true
-      Aji::Video.find(video.id).title.should_not be_nil
-    end
-  end
-  
-  describe "#relevance" do
-    it "should return higher relevance for newer mentions given the same number of mentions" do
-      at_time_i = Time.now.to_i
-      video = Factory :video_with_mentions
-      old_relevance = video.relevance at_time_i
-      
-      # make video's mentions more recent
-      video.mentions.each do |mention|
-        mention.published_at = mention.published_at + rand(100).seconds
-        mention.published_at = Time.now if (mention.published_at.to_i-Time.now.to_i>0)
-        mention.save
-      end
-      video.relevance(at_time_i).should > old_relevance
-    end
-    
-    it "should not consider blacklisted author" do
-      at_time_i = Time.now.to_i
-      video = Factory :video_with_mentions
-      old_relevance = video.relevance at_time_i
+  subject { Aji::Video.new }
 
-      Aji::ExternalAccount.blacklist_id video.mentions.sample.author.id
-      video.relevance(at_time_i).should < old_relevance
+  it "validates the presence of a source"
+  it "validates the presence of an external_id"
+
+  describe "#thumbnail_uri" do
+    it "should be a youtube uri when source is youtube" do
+      video = Aji::Video.new :source => 'youtube', :external_id => "someID12345"
+      video.thumbnail_uri.should =~ %r<^http://img\.youtube\.com/vi/>
     end
+
+    it "should give a valid vimeo thumbnail when the source is vimeo"
+  end
+
+  describe "#populate" do
+    context "when populating a youtube video" do
+      subject do
+        Aji::Video.new :source => 'youtube', :external_id => 'OzVPDiy4P9I'
+      end
+
+      specify "new videos are not populated on creation" do
+        subject.should_not be_populated
+        subject.title.should be_nil
+      end
+
+      specify "videos should have a title and description after #populating" do
+        subject.populate
+        subject.should be_populated
+        subject.title.should_not be_nil
+        subject.description.should_not be_nil
+        subject.author.should_not be_nil
+      end
+    end
+
+    context "when populating a vimeo video" do
+      subject do
+        Aji::Video.new :source => 'vimeo', :external_id => '6968393'
+      end
+
+      specify "new videos are not populated on creation" do
+        subject.should_not be_populated
+        subject.title.should be_nil
+      end
+
+      specify "videos should have a title and description after #populating" do
+        subject.populate
+        subject.should be_populated
+        subject.title.should_not be_nil
+        subject.description.should_not be_nil
+        subject.author.should_not be_nil
+      end
+    end
+  end
+
+  describe "#relevance" do
+    context "two videos with different mention counts" do
+      specify "The video with more mentions is more relevant"
+    end
+   context "two videos with equal mention counts" do
+      specify "The video with more recent mentions is more relevant"
+   end
+
+   it "should not consider blacklisted author" do
+     at_time_i = Time.now.to_i
+     video = Factory :video_with_mentions
+     old_relevance = video.relevance at_time_i
+
+     Aji::ExternalAccount.blacklist_id video.mentions.sample.author.id
+     video.relevance(at_time_i).should < old_relevance
+   end
   end
 end
 
