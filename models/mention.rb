50b72abbd56fe47d8f83c927ca3a65134bb91fcd
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-15 18:57:08 -0700

    Mention converts links to Links on instantiation.

--- models/mention.rb
+++ models/mention.rb
@@ -8,6 +8,16 @@ module Aji
   # - published_at: DateTime
   # - links: Text (serialized to array of Link models)
   class Mention < ActiveRecord::Base
+
+    def initialize params={}
+      super params
+      link_objs = []
+      links.each do |link|
+        link_objs << Link.new(link)
+      end
+      self.links = link_objs
+    end
+
     belongs_to :author, :class_name => 'ExternalAccount'
     has_and_belongs_to_many :videos
 
--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -1,6 +1,18 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Aji::Mention do
+  describe "new mention with specified links" do
+    subject do
+      Aji::Mention.new(
+        :links => [ "http://duckduckgo.com", "http://google.com" ])
+    end
+
+    specify "link strings are converted to Link objects" do
+      subject.links.each do |link|
+        link.class.should == Aji::Link
+      end
+    end
+  end
   describe "links collection" do
     it "defaults to an empty array" do
       subject.links.class.should == Array
