d28fc5bd63c16647206ff50db9c432fdffbcbb2f
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-26 15:44:01 -0700

    Implement find_or_create_by_account and test it.

--- models/channels/twitter_account.rb
+++ models/channels/twitter_account.rb
@@ -3,6 +3,14 @@ module Aji
     belongs_to :account, :class_name => 'Aji::ExternalAccounts::Twitter'
     before_create :set_default_title
 
+
+    # Class methods below
+    def self.find_or_create_by_account account, params={}
+      account.channel ||= Channels::TwitterAccount.create(params.merge(
+        :account => account))
+    end
+
+    # Private instance methods below.
     private
     def set_default_title
       self.title ||= "@#{account.handle}'s Tweeted Videos"
--- spec/models/channels/twitter_account_spec.rb
+++ spec/models/channels/twitter_account_spec.rb
@@ -18,5 +18,30 @@ describe Aji::Channels::TwitterAccount do
         pending "This will be a right cock in the ear to test without VCR"
       end
     end
+
+    describe ".find_or_create_by_account" do
+      before :each do
+        @twitter_user = Aji::ExternalAccounts::Twitter.create :uid => '71231',
+            :user_info => { :nickname => "_nuclearsammich" }
+      end
+      context "when the channel exists" do
+        it "returns the existing channel" do
+          @twitter_user.channel = Aji::Channels::TwitterAccount.create(
+            :account => @twitter_user)
+          Aji::Channels::TwitterAccount.find_or_create_by_account(@twitter_user).
+            should == @twitter_user.channel
+        end
+      end
+
+      context "when the channel doesn't exist" do
+        it "creates a new channel" do
+          expect do
+            Aji::Channels::TwitterAccount.
+              find_or_create_by_account(@twitter_user)
+          end.to change(Aji::Channels::TwitterAccount, :count).by(1)
+
+        end
+      end
+    end
   end
 end
