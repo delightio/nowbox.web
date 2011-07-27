1f0fc6c7bf69d89700904d2431dd8ab7be68eba8
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-27 13:20:52 -0700

    Note to self. Learn why this is.

--- models/channels/twitter_account.rb
+++ models/channels/twitter_account.rb
@@ -1,45 +1,47 @@
 module Aji
-  class Channels::TwitterAccount < Channel
-    belongs_to :account, :class_name => 'Aji::ExternalAccounts::Twitter'
-    before_create :set_default_title
-    after_create :populate
-    TWITTER_API_URL =
-      "http://api.twitter.com/1/statuses/user_timeline.json?count=200&include_entities=true"
+  module Channels
+    class TwitterAccount < Channel
+      belongs_to :account, :class_name => 'Aji::ExternalAccounts::Twitter'
+      before_create :set_default_title
+      after_create :populate
+      TWITTER_API_URL =
+        "http://api.twitter.com/1/statuses/user_timeline.json?count=200&include_entities=true"
 
-    def thumbnail_uri
-      ""
-    end
-
-    # HACK: This is long, complex, blocking, and tightly coupled. A good
-    # candidate for refactoring later.
-    def populate
-      resp = HTTParty.get(
-        TWITTER_API_URL + "&screen_name=#{account.username}")
-      tweets = resp.parsed_response
-      mentions = tweets.map { |tweet| Parsers::Tweet.parse tweet }
-      mentions.map(&:save)
-      mentions.each_with_index do |m, i|
-        m.links.each do |link|
-          next unless link.video?
-          push Video.find_or_create_by_source_and_external_id(
-            link.type, link.external_id)
-        end
+      def thumbnail_uri
+        ""
       end
 
-      # Since we want to use the videos right away we will autopopulate them.
-      content_videos.map(&:populate)
-      update_attribute :populated_at, Time.now
-    end
+      # HACK: This is long, complex, blocking, and tightly coupled. A good
+      # candidate for refactoring later.
+      def populate
+        resp = HTTParty.get(
+          TWITTER_API_URL + "&screen_name=#{account.username}")
+          tweets = resp.parsed_response
+          mentions = tweets.map { |tweet| Parsers::Tweet.parse tweet }
+          mentions.map(&:save)
+          mentions.each_with_index do |m, i|
+            m.links.each do |link|
+              next unless link.video?
+              push Video.find_or_create_by_source_and_external_id(
+                link.type, link.external_id)
+            end
+          end
 
-    # Class methods below
-    def self.find_or_create_by_account account, params={}
-      account.channel ||= Channels::TwitterAccount.create(params.merge(
-        :account => account))
-    end
-    # Private instance methods below.
-    private
-    def set_default_title
-      self.title ||= "@#{account.username}'s Tweeted Videos"
+          # Since we want to use the videos right away we will autopopulate them.
+          content_videos.map(&:populate)
+          update_attribute :populated_at, Time.now
+      end
+
+      # Class methods below
+      def self.find_or_create_by_account account, params={}
+        account.channel ||= Channels::TwitterAccount.create(params.merge(
+          :account => account))
+      end
+      # Private instance methods below.
+      private
+      def set_default_title
+        self.title ||= "@#{account.username}'s Tweeted Videos"
+      end
     end
   end
 end
