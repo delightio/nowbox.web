d71d7f73ac46c049b3a45263d02a863a3297e2a1
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-27 12:32:21 -0700

    Use push method and update videos on populate.

--- models/channels/twitter_account.rb
+++ models/channels/twitter_account.rb
@@ -5,37 +5,36 @@ module Aji
     TWITTER_API_URL =
       "http://api.twitter.com/1/statuses/user_timeline.json?count=50&include_entities=true"
 
+    def thumbnail_uri
+      ""
+    end
+
+    # HACK: This is long, complex, blocking, and tightly coupled. A good
+    # candidate for refactoring later.
+    def populate
+      resp = HTTParty.get(
+        TWITTER_API_URL + "&screen_name=#{account.username}")
+      tweets = resp.parsed_response
+      mentions = tweets.map { |tweet| Parsers::Tweet.parse tweet }
+      mentions.map(&:save)
+      mentions.each_with_index do |m, i|
+        m.links.each do |link|
+          next unless link.video?
+          push Video.find_or_create_by_source_and_external_id(
+            link.type, link.external_id)
+        end
+      end
+
+      # Since we want to use the videos right away we will autopopulate them.
+      content_videos.map(&:populate)
+      update_attribute :populated_at, Time.now
+    end
 
     # Class methods below
     def self.find_or_create_by_account account, params={}
       account.channel ||= Channels::TwitterAccount.create(params.merge(
         :account => account))
     end
-
-    def thumbnail_uri
-      ""
-    end
-
-    # HACK: This is long, complex, and tightly coupled. A good later refactor
-    # candidate.
-    def populate
-      puts account.username
-      resp = HTTParty.get(
-        TWITTER_API_URL + "&screen_name=#{account.username}")
-      tweets = resp.parsed_response
-      puts "Tweets:#{tweets.class} => #{tweets.inspect}"
-      puts "Firsttweet:#{tweets.first.class} => #{tweets.first.inspect}"
-      mentions = tweets.map { |tweet| Parsers::Tweet.parse tweet }
-      mentions.map(&:save)
-      mentions.each_with_index do |m, i|
-        m.links.each do |link|
-          next unless link.video?
-          content_zset[Video.find_or_create_by_source_and_external_id(
-            link.type, link.external_id)] = i
-        end
-      end
-    end
-
     # Private instance methods below.
     private
     def set_default_title
