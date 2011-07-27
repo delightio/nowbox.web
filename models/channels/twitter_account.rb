192e065aba19d93e853c9d7c7be9726cc5df551f
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-27 09:02:45 -0700

    Implement blocking populate method for Twitter channel.

--- models/channels/twitter_account.rb
+++ models/channels/twitter_account.rb
@@ -2,6 +2,8 @@ module Aji
   class Channels::TwitterAccount < Channel
     belongs_to :account, :class_name => 'Aji::ExternalAccounts::Twitter'
     before_create :set_default_title
+    TWITTER_API_URL =
+      "http://api.twitter.com/1/statuses/user_timeline.json?count=50&include_entities=true"
 
 
     # Class methods below
@@ -12,6 +14,23 @@ module Aji
 
     def thumbnail_uri
       ""
+    end
+
+    # HACK: This is long, complex, and tightly coupled. A good later refactor
+    # candidate.
+    def populate
+      tweets_json= HTTParty.get(
+        TWITTER_API_URL + "&screen_name=#{account.username}")
+      tweets = MultiJson.decode tweets_json
+      mentions = tweets.map { |tweet| Parsers::Tweet.parse tweet }
+      mentions.map(&:save)
+      mentions.each_with_index do |m, i|
+        mention.links.each do |link|
+          next unless link.video?
+          content_zset[Video.find_or_create_by_source_and_external_id(
+            link.source, link.external_id)] = i
+        end
+      end
     end
 
     # Private instance methods below.
