2bf6224f585e8d7b0b4a1e7de74feb011789c043
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-07-31 13:03:54 -0700

    Update Aji.log and comment docs.

--- aji.rb
+++ aji.rb
@@ -8,17 +8,23 @@ module Aji
   # Set the application root directory.
   def Aji.root; File.expand_path('..', __FILE__); end
 
-  # Simple logging interface for local development and heroku. May grow in
-  # complexity later, hence methodizing it.
+  # Logging interface for local development and heroku.
+  # There are four internal Log levels aliased to the standard SYSLOG levels.
+  # The INFO level is used if no level is specified. Other options are `:DEBUG`,
+  # for DEBUG and `:WARN`, `:FATAL`, `:ERROR` for ERROR as well as a `:WTF`
+  # option for Really Bad Things. downcased versions of these work as well.
+  # DEBUG logs are not logged in production environments so conditional logging
+  # should not be used.
   def Aji.log level=:INFO, message
     case level
     when :ERROR, :WARN, :FATAL, :error, :warn, :fatal
       $stderr.puts message
     when :DEBUG, :debug
+      return if RACK_ENV == 'production'
       $stdout.puts '----------DEBUG----------', message,
         '----------DEBUG----------'
     when :WTF, :wtf
-      puts "!!!!!!!!!!!!!!!!!!!! LOOOK AT ME DAMMIT !!!!!!!!!!!!!!!!!!!!",
+      $stderr.puts "!!!!!!!!!!!!!!!!!!!! LOOOK AT ME DAMMIT !!!!!!!!!!!!!!!!!!!!",
            "!!!!!!!!!!!!!!!!!!!! I AM NOT RIGHT MAN !!!!!!!!!!!!!!!!!!!!",
            message,
            "!!!!!!!!!!!!!!!!!!!!   FOR FUCK'S SAKE   !!!!!!!!!!!!!!!!!!!",
