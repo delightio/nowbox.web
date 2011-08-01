38a690d3191baaf513eb7f314898cafe1cec87b8
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-01 16:41:05 -0700

    Use predefined script and make it directly runnable.

--- script/populate_predefined_channels.rb
+++ script/populate_predefined_channels.rb
@@ -1,3 +1,5 @@
+require_relative '../aji'
+
 jsonfilename = "#{Aji.root}/config/channels.json"
 json = File.open jsonfilename
 channels_json = JSON.parse json.read
@@ -5,7 +7,7 @@ raise "Can't parse #{jsonfilename}" if channels_json.nil?
 channels_json.each do |ch|
   puts "Creating #{ch["title"]} channel with #{ch["usernames"].count} #{ch["type"]} accounts: #{ch["usernames"]}..."
   raise "Can't deal with type #{ch["type"]}" if ch["type"]!="youtube"
-  
+
   start = Time.now
   channel = Aji::Channels::YoutubeAccount.find_or_create_by_usernames(
     ch["usernames"],
