0d4e1126184c364de8e3777f9992b5026ec830ae
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-04 14:29:44 -0700

    Temporarily remove index migrations.

--- db/migrate/20110803193828_add_index_to_external_accounts.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToExternalAccounts < ActiveRecord::Migration
-  def self.up
-    add_index(:external_accounts, [:uid, :type], :unique => true)
-  end
-
-  def self.down
-    remove_index :external_accounts, :column => [:uid, :type]
-  end
-end
--- db/migrate/20110803234009_add_index_to_videos.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexToVideos < ActiveRecord::Migration
-  def self.up
-    add_index(:videos, [:external_id, :source], :unique => true)
-  end
-
-  def self.down
-    remove_index :videos, :column => [:external_id, :source]
-  end
-end
