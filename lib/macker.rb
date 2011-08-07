7c08ee780ce84e8222f0984f05fa12b5e8abc547
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-07 12:10:00 -0700

    Remove unneeded attr_reader.

--- lib/macker.rb
+++ lib/macker.rb
@@ -1,7 +1,6 @@
 module Aji
   module Macker
     autoload :Youtube, "#{Aji.root}/lib/macker/youtube"
-    attr_reader :sources
 
     # TODO: Is there a more interesting way than Case to solve this?
     def Macker.fetch source, source_id
