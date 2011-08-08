1aab552960783e56e5ff7a6971f7412e737e524a
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-08 15:24:01 -0700

    Update queues with new class name.

--- queues/publish_share.rb
+++ queues/publish_share.rb
@@ -3,7 +3,7 @@ module Queues
     @queue = :publish_share
 
     def self.perform external_account_id, share_id
-      acc = ExternalAccount.find(external_account_id)
+      acc = Account.find(external_account_id)
       share = Share.find(share_id)
 
       acc.publish share.message
