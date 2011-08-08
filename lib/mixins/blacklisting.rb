6a3f15b3cd7a58a5d40e342ee0704cef703f6006
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-08 12:24:28 -0700

    Fix whitespace.

--- lib/mixins/blacklisting.rb
+++ lib/mixins/blacklisting.rb
@@ -1,17 +1,17 @@
 module Aji
   module Mixins
     module Blacklisting
-      
+
       # Require a 'blacklisted_at' DateTime database column
-      
-      def blacklist
+
+     def blacklist
         update_attribute :blacklisted_at, Time.now
       end
-      
+
       def blacklisted?
         !blacklisted_at.nil?
       end
-      
     end
   end
 end
+
