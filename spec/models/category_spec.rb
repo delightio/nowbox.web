49a9106d1ef363489e01ece5acf4ac07fa380a55
Thomas Pun <tpun@ottomator.com>
2011-08-10 00:19:58 -0700

    fixed missing (

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 describe Aji::Category do
-  subject { Aji::Category.create
-    :title => random_string, :raw_title => random_string }
+  subject { Aji::Category.create(
+    :title => random_string, :raw_title => random_string) }
 
   it "has an id, title, label and a list of associated channels" do
     20.times { |n| Factory :youtube_channel }
@@ -14,9 +14,10 @@ describe Aji::Category do
       Aji::Channel.find(cid).should_not be_nil
     end
   end
-  
+
   it "sets title as raw_title after create" do
     c = Aji::Category.find_or_create_by_raw_title random_string
     c.title.should_not be_nil
   end
+
 end
