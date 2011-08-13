48ce9f3c9cf9c49537a02f0fe39810b82a8d8f92
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-13 13:24:24 -0700

    Add Graph channel creation.

--- models/identity.rb
+++ models/identity.rb
@@ -2,6 +2,14 @@ class Aji::Identity < ActiveRecord::Base
   has_many :accounts, :class_name => 'Aji::Account'
   has_one :user, :class_name => 'Aji::User'
 
+  # TODO: Add static link to graph channel since it is one of the few mutable
+  # channels, we'll want to keep track of these to keep things from spiralling
+  # out of control.
+  def graph_channel
+    Channel::Account.find_or_create_by_accounts :accounts => accounts,
+      :title => "#{user.first_name}'s Social Channel"
+  end
+
   # Identity is a model which associates multiple user accounts with the same
   # person or corporation. At this time, the only way for this to happen is for
   # a user to log in and authorize us for multiple services (such as Twitter,
