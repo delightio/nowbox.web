29ab9d462c160f24dd4d39799965152b9b996770
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-17 15:25:58 -0700

    Disconnect after perform.

--- queues/with_database_connection.rb
+++ queues/with_database_connection.rb
@@ -4,6 +4,10 @@ module Aji
       def after_fork_connect_to_database *args
         ActiveRecord::Base.establish_connection Aji.conf['DATABASE']
       end
+
+      def after_perform_disconnect_db *args
+        ActiveRecord::Base.connection.disconnect!
+      end
     end
   end
 end
