226ffbe2bc0ba91c6dbe92f7f5fc04c1246a6bf9
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-17 14:19:07 -0700

    Add logging method to queue.

--- queues/with_database_connection.rb
+++ queues/with_database_connection.rb
@@ -2,9 +2,12 @@ module Aji
   module Queues
     module WithDatabaseConnection
       def self.around_perform
+        Aji.log "Connecting to Database"
         ActiveRecord::Base.establish_connection Aji.conf['DATABASE']
         yield
+        Aji.log "Closing connection to Database"
         ActiveRecord::Base.connection.disconnect!
+        Aji.log "Database connection closed"
       end
     end
   end
