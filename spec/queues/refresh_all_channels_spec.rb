e6fdd530ceb2e4bc76afea7b6e341420355a7c67
Thomas Pun <tpun@ottomator.com>
2011-08-18 02:28:24 -0700

    remove white space and code

--- spec/queues/refresh_all_channels_spec.rb
+++ spec/queues/refresh_all_channels_spec.rb
@@ -5,33 +5,33 @@ module Aji
     before(:each) do
       @total = 10
       @channels = []
-      @total.times { |n| @channels << (Factory :youtube_channel_with_videos) }
+      @total.times { @channels << (Factory :youtube_channel_with_videos) }
     end
     describe ".perform" do
       it "calls #populate on all existing channels" do
         Resque.should_receive(:enqueue).exactly(@total).times
         Queues::RefreshAllChannels.perform
       end
-      
+
       it "returns if automatic population is turned off" do
         Queues::RefreshAllChannels.stub(:automatically?).and_return(false)
         Resque.should_receive(:enqueue).never
         Queues::RefreshAllChannels.perform
       end
     end
-  
+
     describe ".automatically?" do
       it "returns true if no flag is set" do
         Aji.stub(:conf).and_return(Hash.new)
         Queues::RefreshAllChannels.automatically?.should == true
       end
-      
+
       it "returns true if flag is set to false" do
         h = { 'PAUSE_AUTOMATIC_CHANNEL_POPULATION' => false }
         Aji.stub(:conf).and_return(h)
         Queues::RefreshAllChannels.automatically?.should == true
       end
-    
+
       it "only returns false if flag is set" do
         h = { 'PAUSE_AUTOMATIC_CHANNEL_POPULATION' => true }
         Aji.stub(:conf).and_return(h)
