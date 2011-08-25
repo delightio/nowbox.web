40563cea4bef7f4e8bf3acf81fe2ef246e8d0283
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-25 15:15:04 -0700

    Remove vimeo support for now.

--- models/link.rb
+++ models/link.rb
@@ -43,7 +43,7 @@ module Aji
         self.match r || false
       end.inject { |acc, el| acc ||= el }
       # TODO: Finish implementing vimeo support.
-      vimeo_match = self.match @@vimeo_regexp
+      vimeo_match = nil#self.match @@vimeo_regexp
       if youtube_match
         @external_id = youtube_match[1]
         @type = 'youtube'
