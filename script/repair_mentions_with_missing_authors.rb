d9f57c9abe85f8495a8661aecd17b8b5c04ba389
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-26 10:44:26 -0700

    Add script to find and repair mentions with nil authors.

---
+++ script/repair_mentions_with_missing_authors.rb
@@ -0,0 +1,28 @@
+require_relative '../aji'
+
+Aji.log "Repairing Mentions with Nil Authors"
+
+nil_author_mentions = Aji::Mention.where(:author_id => nil).select(
+  [ :id, :unparsed_data ])
+
+Aji.log :WARN, "Found #{nil_author_mentions.count} mentions with nil authors."
+
+nil_author_mentions.each do |m|
+  user = MultiJson.decode(m.unparsed_data)['user']
+  account = Aji::Account::Twitter.find_by_username user['screen_name']
+  unless account.nil?
+    Aji.log :WARN, "#{account.username} existed in DB but wasn't linked to " +
+      m.id
+  else
+    Aji.log :WARN, "No account was created for #{user['screen_name']}."
+    account = Aji::Account::Twitter.create :username => user['screen_name'],
+      :uid => user['id'], :info => user
+  end
+
+  m.author = account
+  if m.save
+    Aji.log "Linked #{m.id} with #{account.id}"
+  else
+    Aji.log :WTF, "Unable to link #{m.id} with #{account.id} please check!"
+  end
+end
