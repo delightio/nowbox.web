066f9c85bef3fe9d31ee4af9474e5488070e0bdd
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-08-26 21:56:20 -0700

    Quick script to find and remove duplicates.

---
+++ script/fix_duplicate_accounts.rb
@@ -0,0 +1,20 @@
+require_relative '../aji'
+
+Aji.log "Fixing duplicate accounts"
+count_hash = Hash.new(0)
+accounts = Aji::Accounts.select(:username).map(&:username).each do |u|
+  count_hash[u] += 1
+end
+
+count_hash.each do |count, username|
+  next if count < 2
+  duped_accounts = Aji::Accounts.find_all_by_username username
+  keep_me = duped_accounts.first
+  duped_accounts[1..-1].each do |dupe|
+    keep_me.mentions += dupe.mentions
+    keep_me.save
+    dupe.destroy
+    Aji.log "Deleted Account[#{dupe.id}] with #{dupe.mentions.count} mentions" +
+      " transfered to Account[#{keep_me.id}]"
+  end
+end
