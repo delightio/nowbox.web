99354ebe4292f30cb4eaff227e580a8062ce2840
Thomas Pun <tpun@ottomator.com>
2011-09-02 22:14:26 -0700

    update script to enqueue channel refresh. updated channels.json [#290 state:resolved]

--- script/populate_predefined_channels.rb
+++ script/populate_predefined_channels.rb
@@ -7,6 +7,7 @@ jsonfilename = "#{Aji.root}/config/channels.json"
 json = File.open jsonfilename
 channels_json = JSON.parse json.read
 raise "Can't parse #{jsonfilename}" if channels_json.nil?
+channels = []
 channels_json.each do |ch|
   puts "Creating #{ch["title"]} channel with #{ch["usernames"].count} #{ch["type"]} accounts: #{ch["usernames"]}..."
   raise "Can't deal with type #{ch["type"]}" if ch["type"]!="youtube"
@@ -15,12 +16,26 @@ channels_json.each do |ch|
     Aji::Account::Youtube.find_or_create_by_uid u
   end
 
-  start = Time.now
   channel = Aji::Channel::Account.find_or_create_by_accounts(
-    accounts, { :title => ch["title"], :default_listing => true }, true)
-  puts "  => #{channel.inspect} in #{Time.now-start} s."
+    accounts, { :title => ch["title"], :default_listing => true })
+  puts "  => #{channel.inspect}"
   if channel.save != true
     puts "*** error saving #{channel.title} because: #{channel.errors}"
   end
+  channels << channel
+  Resque.enqueue Aji::Queues::RefreshChannel, channel.id
   puts
+
+  # Also create individial
+  accounts.each do |account|
+    channel = Aji::Channel::Account.find_or_create_by_accounts [account]
+    channels << channel
+    puts "Created #{channel.accounts.first.username}'s channel: #{channel.inspect}"
+  end
+  puts
+
 end
+
+puts
+puts "*** Created #{channels.count} channels. Enqueuing them for refresh..."
+channels.each {|c| Resque.enqueue Aji::Queues::RefreshChannel, c.id}
