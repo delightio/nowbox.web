2a6abc8df417aa1975a716bd4acfdc2e100356d9
Thomas Pun <tpun@ottomator.com>
2011-09-09 00:28:41 -0700

    don't do anythign in examine video

--- queues/examine_video.rb
+++ queues/examine_video.rb
@@ -8,6 +8,8 @@ module Aji
       def self.perform args
         video = Video.find_by_id args[:video_id]
         unless video.nil?
+Aji.log "Video[#{video.id}] getting blacklisted in ExamineVideo"
+return
           video.blacklist
           bad_count = Video.where(
             "author_id = ? AND blacklisted_at IS NOT NULL", video.author.id).
