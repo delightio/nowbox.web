7528de453732859c27e2d191383cbbb6828a0642
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-09 17:30:10 -0700

    For now only log and store spammer ids.

--- queues/remove_spammer.rb
+++ queues/remove_spammer.rb
@@ -4,10 +4,15 @@ module Aji
       extend WithDatabaseConnection
 
       @queue = :remove_spammer
+      @spammer_set_key = "spammers"
 
       def self.perform spammer_id
         spammer = Account.find spammer_id
-        spammer.mark_spammer
+        spammer.blacklist
+        Aji.log "* spammer * Account[#{spammer.id}] was found to be spammy.\n" +
+          "You can find them and all other spammers in the redis set " +
+          "'#{@spammer_set_key}'"
+        Aji.redis.sadd @spammer_set_key, spammer.id
       end
     end
   end
