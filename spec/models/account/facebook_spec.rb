fcf3c038ad5bd1832ffaa1b520762c156dc69958
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-15 17:48:04 -0700

    Add ability to fetch data from facebook.

--- spec/models/account/facebook_spec.rb
+++ spec/models/account/facebook_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 module Aji
   describe Account::Facebook do
-    subject { Account::Facebook.create uid: 501776555 }
+    subject { Account::Facebook.create uid: "501776555" }
     it_behaves_like "any account"
   end
 end
