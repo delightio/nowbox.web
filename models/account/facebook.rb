0d9071c09619686ffdcebf9578165f51854ec4d3
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-21 14:22:56 -0700

    Can get videos posted by authorized facebook users.

--- models/account/facebook.rb
+++ models/account/facebook.rb
@@ -6,9 +6,29 @@ module Aji
     validates_presence_of :uid
     validates_uniqueness_of :uid
 
+    # Use an alias to allow Facebook accounts to implement the same protocal as
+    # Channel::Trending without the need for a recent_zset buffer.
+    alias_method :push_recent, :push
+
     def refresh_content force=false
       super force do |new_videos|
+        links = api.get_connections "me", "links"
+        # TODO: smelly
+        new_videos.concat videos_from_graph_collection links
+        new_videos.concat videos_from_graph_collection links.next_page
+        new_videos.each { |v| v.populate }
+        Aji.log "Found #{new_videos.count} in facebook links."
+      end
+    end
 
+    def videos_from_graph_collection links
+      videos = [].tap do |videos|
+        links.each do |link|
+          mention = Parsers[:facebook].parse link
+          processor = Mention::Processor.new mention, self
+          processor.perform
+          videos.concat processor.found_videos
+        end
       end
     end
 
@@ -23,5 +43,15 @@ module Aji
     def profile_uri
       info['link']
     end
+
+    def authorized?
+      credentials.key? 'token'
+    end
+
+    private
+    def api
+      @api ||= Koala::Facebook::API.new credentials['token']
+    end
+
   end
 end
--- spec/models/account/facebook_spec.rb
+++ spec/models/account/facebook_spec.rb
@@ -2,7 +2,11 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 module Aji
   describe Account::Facebook do
-    subject { Account::Facebook.create uid: "501776555" }
+    subject do
+      Account::Facebook.create uid: "501776555", credentials: { 'token' =>
+        "AAACF78hfSZBEBAM0leS4CSzXZARd7S68Al6uVzs8DwJ8huZAm1YsjYeiZA2gBR3p7Ue8l3EPrKjkv6EtmOQuXo95aNTIcPIZD" }
+    end
+
     it_behaves_like "any account"
   end
 end
