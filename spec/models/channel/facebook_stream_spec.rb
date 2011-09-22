0f96e442b48a07cad09d640410fec0a93f23918a
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-22 15:14:06 -0700

    This spec is broken due to activerecord. I cry.

--- spec/models/channel/facebook_stream_spec.rb
+++ spec/models/channel/facebook_stream_spec.rb
@@ -2,7 +2,16 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 module Aji
   describe Channel::FacebookStream do
-    subject { Channel::FacebookStream.create }
+    before :each do
+      # TODO: This is pretty indicative that #refresh_content needs to be
+      # refactored.
+      video = mock "video", :populate => true, :populated? => true
+      mention = mock "mention", :videos => Array.new(1, @video)
+      api = mock "FacebookAPI"
+      api.stub(:video_mentions_in_feed).and_return Array.new 3, mention
+      @owner = mock "Facebook Account", :api => @api
+    end
+    subject { Channel::FacebookStream.create :owner => @owner }
     it_behaves_like "any channel"
   end
 end
