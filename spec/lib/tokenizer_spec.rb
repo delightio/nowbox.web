6ad1698ed4a544672343d8b62c0474fb23698906
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-24 13:16:37 -0700

    Tweak some specs for better isolation.

--- spec/lib/tokenizer_spec.rb
+++ spec/lib/tokenizer_spec.rb
@@ -1,23 +1,23 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
-describe "String#tokenize" do
-  subject { Array.new(10){|n| random_string }.join(' , ') }
-  
+describe "String#tokenize", unit: true do
+  subject { "Shoulder,est ,mollit, sint,  Occaecat,deserunt,beef" }
+
   it "removes leading and trailing space around token" do
     token = subject.tokenize.sample
     token.should == token.strip
   end
-  
+
   it "removes stopwords" do
     stopwords = %w[ the ]
     stopword = stopwords.sample
     str = "#{subject}, #{stopword}"
     str.tokenize.should_not include stopword
   end
-  
+
   it "returns array of tokens which are all lower case" do
     tokens = subject.tokenize
-    tokens.should have(10).tokens
-    tokens.each {|t| t.downcase.should == t }
+    tokens.should have(7).tokens
+    tokens.each { |t| t.downcase.should == t }
   end
 end
