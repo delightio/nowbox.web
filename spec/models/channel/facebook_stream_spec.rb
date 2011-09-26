871bef652ce49ddd79156d7494186f7b35aed5fb
Thomas Pun <tpun@ottomator.com>
2011-09-26 11:03:35 -0700

    fix spec

--- spec/models/channel/facebook_stream_spec.rb
+++ spec/models/channel/facebook_stream_spec.rb
@@ -2,13 +2,12 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 module Aji
   describe Channel::FacebookStream do
-    pending
-  #   before :each do
-  #     @owner = Account::Facebook.create uid: "501776555", credentials:
-  #       { 'token' => "AAACF78hfSZBEBAM0leS4CSzXZARd7S68Al6uVzs8DwJ8huZAm1YsjYeiZA2gBR3p7Ue8l3EPrKjkv6EtmOQuXo95aNTIcPIZD" }
-  #   end
-  # 
-  #   subject { Channel::FacebookStream.create :owner => @owner }
-  #   it_behaves_like "any channel"
-  # end
+    # before :each do
+    #   @owner = Account::Facebook.create uid: "501776555", credentials:
+    #     { 'token' => "AAACF78hfSZBEBAM0leS4CSzXZARd7S68Al6uVzs8DwJ8huZAm1YsjYeiZA2gBR3p7Ue8l3EPrKjkv6EtmOQuXo95aNTIcPIZD" }
+    # end
+    #   
+    # subject { Channel::FacebookStream.create :owner => @owner }
+    # it_behaves_like "any channel"
+  end
 end
