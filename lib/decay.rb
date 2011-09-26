5c694fb50982cf019773c05f40523f00b93c27e4
Thomas Pun <tpun@ottomator.com>
2011-09-26 14:08:31 -0700

    increase half life time span to 1 hour [#448, #482]

--- lib/decay.rb
+++ lib/decay.rb
@@ -3,7 +3,7 @@ module Aji
     # Exponential decay
     # Rel' = Rel * exp(-k*time_diff/half_life)
     # When time_diff = half_life and Rel' = 0.5*Rel => -k = log(0.5) = -0.69314718
-    def self.exponentially time_diffs, half_life=30.minutes, value_at_t0=10000
+    def self.exponentially time_diffs, half_life=1.hour, value_at_t0=10000
       time_diffs.inject(0) do | sum, time_diff |
         next 0 unless time_diff.integer?
         sum + (value_at_t0 * Math.exp(-0.69314718*time_diff/half_life))
