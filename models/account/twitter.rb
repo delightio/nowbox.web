ee57d497603beadc134bbf513b4bcd564fef90cd
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-27 08:33:58 -0700

    Isolate Account::Twitter spec.

--- models/account/twitter.rb
+++ models/account/twitter.rb
@@ -13,7 +13,7 @@ module Aji
     after_create :set_provider
 
     def existing?
-      false
+      api.valid_uid? uid
     end
 
     def profile_uri
@@ -31,29 +31,24 @@ module Aji
 
     def refresh_content force=false
       super force do |new_videos|
-        harvest_tweets
-        videos = recent_video_ids.map { |id| Aji::Video.find_by_id id }.
-          select { |v| not (v.nil? || v.blacklisted?) }
-        Aji.log "Found #{videos.count} videos in #{username}'s Twitter stream"
-
-        videos.each do |video|
-          video.populate unless video.populated?
-          push(video, recent_zset[video.id]) and
-            new_videos << video if video.populated?
+        mentions = api.video_mentions_i_post
+        mentions.each do |m|
+          m.videos.each do |v|
+            v.populate do |video|
+              new_videos << video
+              push video, m.published_at
+            end
+          end
         end
       end
     end
 
     def publish share
-      authorize_with_twitter! do
-        Twitter.update format_for_twitter(share.message, share.link)
-        share.published_to << :twitter
-        share.save || puts("Could not save #{share.inspect}")
-      end
+      api.publish format share.message
     end
 
-    def format_for_twitter message, link
-      coda = " #{share.link} via @nowbox for iPad"
+    def format message, link
+      coda = " #{link} via @nowbox for iPad"
       if (message + coda).length > 140
         message[0..message.length - (3 + coda.length)] << "..." << coda
       else
@@ -84,33 +79,12 @@ module Aji
 
     def mark_spammer
       Aji.redis.sadd "spammers", id
-      mentions.map { |m| m.mark_spam }
+      mentions.each { |m| m.mark_spam }
       blacklist
     end
 
     def authorized?
       credentials.has_key? 'token' and credentials.has_key? 'secret'
-    end
-
-    # This method authorizes the global twitter account to act on behalf of this
-    # user. If the optional block is given then after running the block the
-    # client will be deauthorized. Otherwise this will modify the state of the
-    # global twitter client.
-    def authorize_with_twitter!
-      fail "No credentials for #{username} (Account[#{id}])" unless authorized?
-      ::Twitter.configure do |c|
-        c.consumer_key = Aji.conf['CONSUMER_KEY']
-        c.consumer_secret = Aji.conf['CONSUMER_SECRET']
-        c.oauth_token = credentials['token']
-        c.oauth_token_secret = credentials['secret']
-      end
-      if block_given?
-        yield
-        ::Twitter.configure do |c|
-          c.oauth_token = nil
-          c.oauth_token_secret = nil
-        end
-      end
     end
 
     private
@@ -137,6 +111,10 @@ module Aji
     rescue ::Twitter::BadGateway, ::Twitter::InternalServerError,
       ::Twitter::ServiceUnavailable => e
       Aji.log :WARN, "#{e.class}: #{e.message}"
+    end
+
+    def api
+      @api ||= TwitterAPI.new credentials['token'], credentials['secret']
     end
 
     def set_provider
--- spec/models/account/twitter_spec.rb
+++ spec/models/account/twitter_spec.rb
@@ -1,31 +1,88 @@
 require File.expand_path("../../../spec_helper", __FILE__)
 
-describe Aji::Account::Twitter do
-  subject { Aji::Account::Twitter.create(:uid => '178492493',
-                                      :username => '_nuclearsammich') }
+module Aji
+  describe Aji::Account::Twitter, :unit, :focus do
+    let(:video) do
+      mock("video").tap do |v|
+        v.stub :id => 7
 
-  it_behaves_like "any account"
-
-  describe "#refresh_influencers" do
-    it "adds twitter followers as influencers" do
-      expect { subject.refresh_influencers }.to change(subject,
-        :influencer_ids).from([])
-    end
-  end
-
-  describe "#mark_spammer" do
-    it "marks own mentions as spam and destroys them" do
-      videos = mock("video collection")
-      videos.stub(:map).and_return([])
-      mention = mock("mention", :id => 9, :spam? => true, :videos => videos)
-      mention.should_receive :mark_spam
-      subject.stub(:mentions).and_return([mention])
-      subject.mark_spammer
+        def v.populate
+          yield self
+        end
+      end
     end
 
-    it "blacklists self" do
-      subject.should_receive(:blacklist)
-      subject.mark_spammer
+    let(:api) do
+      mock "api", :video_mentions_i_post => [
+        stub(:published_at => Time.now,
+         :videos => [video]) ]
+    end
+
+    subject do
+      Account::Twitter.create(:uid => '178492493').tap do |a|
+        a.stub :api => api
+      end
+    end
+
+    #it_behaves_like "any content holder"
+    it_behaves_like "any redis object model"
+
+    describe "#refresh_influencers" do
+      xit "adds twitter followers as influencers" do
+      end
+    end
+
+    describe "#mark_spammer" do
+      let(:mentions) { stub.as_null_object }
+
+      before do
+        subject.stub(:mentions).and_return mentions
+      end
+
+      it "marks own mentions as spam and destroys them" do
+        mentions.should_receive :each
+        subject.mark_spammer
+      end
+
+      it "blacklists itself" do
+        subject.should_receive :blacklist
+        subject.mark_spammer
+      end
+    end
+
+    describe "#authorized?" do
+      it "is true when user has token and secret credentials" do
+        subject.stub(:credentials).and_return({ 'token' => "tokenstring",
+          'secret' => "secretstring" })
+        subject.should be_authorized
+      end
+
+      it "is false when account has no credentials" do
+        subject.stub(:credentials).and_return Hash.new
+        subject.should_not be_authorized
+      end
+    end
+
+    describe "#format" do
+      let(:long_message) { "A really #{"long" * 15} string" }
+      let(:short_message) { "Hah this video!" }
+      let(:link_text) { "http://nowbox.com/share/51232" }
+
+      it "shortens messages to less than 140 characters" do
+        subject.format(long_message, link_text).length.should be < 140
+        subject.format(short_message, link_text).length.should be < 140
+      end
+
+      it "includes the link text no matter the message length" do
+        subject.format(long_message, link_text).should include link_text
+        subject.format(short_message, link_text).should include link_text
+      end
+
+      it "includes the twitter coda" do
+        coda = " #{link_text} via @nowbox for iPad"
+        subject.format(long_message, link_text).should include coda
+        subject.format(short_message, link_text).should include coda
+      end
     end
   end
 end
