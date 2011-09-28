f204c945738cd60c7da03cf06630220881ad5b81
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-28 15:23:35 -0700

    Revert Account::Facebook to the old API.

--- spec/models/facebook_api_spec.rb
+++ spec/models/facebook_api_spec.rb
@@ -5,7 +5,6 @@ module Aji
     subject { FacebookAPI.new "AAACF78hfSZBEBAM0leS4CSzXZARd7S68Al6uVzs8DwJ8huZAm1YsjYeiZA2gBR3p7Ue8l3EPrKjkv6EtmOQuXo95aNTIcPIZD" }
     describe "#video_mentions_in_feed" do
       it "returns mentions with videos" do
-        pending
         mentions = subject.video_mentions_in_feed
         mentions.each do |m|
           m.class.should_equal Mention, "Expected #{m.inspect} to be a Mention."
