8147374ae241139921685d32c558723840ca9b7c
Thomas Pun <tpun@ottomator.com>
2011-09-28 02:44:27 -0700

    update featured order

--- script/set_featured.rb
+++ script/set_featured.rb
@@ -1,7 +1,7 @@
 require_relative '../aji'
 
-featured_by_title = %w(Comedy Music News Games Sports Trailers Entertainment Tech Film Education Travel Howto Autos )
+featured_by_title = %w(Comedy News Games Sports Trailers Music Entertainment Film Tech Education Travel Howto Autos )
 Aji::Category.set_featured featured_by_title
 
-featured_by_title = %w( NowPopular NowComedy NowNews aplusk freddiew )
+featured_by_title = %w( NowComedy NowNews NowPopular freddiew )
 Aji::Channel.set_featured featured_by_title
