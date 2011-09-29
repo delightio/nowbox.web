180978289a4be5774d95b628b57afec746c13ed6
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-09-29 10:39:02 -0700

    Add realname to YoutubeAPI spec.

--- spec/models/youtube_api.rb
+++ spec/models/youtube_api.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
 module Aji
-  describe Aji::YoutubeAPI do
+  describe Aji::YoutubeAPI, :unit, :net do
     describe "#author_info" do
       it "gets info from youtube" do
         info = subject.author_info 'day9tv'
@@ -10,7 +10,26 @@ module Aji
 Watch my video autobiography here: http://www.youtube.com/watch?v=NJztfsXKcPQ)
         info.profile_uri.should == "http://www.youtube.com/profile?user=day9tv"
         info.thumbnail_uri.should == "http://i2.ytimg.com/i/axar6TBM-94_ezoS00fLkA/1.jpg?v=b5d95a"
-        info.real_name.should == "Sean Day[9] Plott Plott"
+        info.realname.should == "Sean Day[9] Plott Plott"
+      end
+    end
+
+    describe "#video_info", :focus do
+      let(:video_info) do {
+        :duration => 901,
+        :view_count => 54814,
+        :blacklisted_at => nil,
+        :published_at => Time.parse("2011-04-23 16:16:48 UTC"),
+        :source => "youtube",
+        :populated_at => "2011-09-28 15:34:17 -0700"
+      }
+      end
+
+      it "gives information about a video in a hash" do
+        hash = subject.video_info '3307vMsCG0I'
+        hash[:title].should == "[Portal 2] Corrupt Core Quotes (Space, Fact and Adventure Spheres)"
+        hash[:external_id].should == "3307vMsCG0I"
+        hash[:description].should == "Here are all the lines for the corrupt cores during the final fight scene. Not gunna lie, i couldnt stop laughing during the final battle because of these little bastards.   Anyways, Enjoy, Comment, Rate, Subscribe, Share! :D"
       end
     end
   end
