a9fc11dc80d9d1fc2fd94cca0400aa80c4649287
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-01 13:38:45 -0700

    Add TwitterStream model.

---
+++ models/channel/twitter_stream.rb
@@ -0,0 +1,22 @@
+module Aji
+  class Channel::TwitterStream < Channel
+
+   has_one :owner, :class_name => 'Aji::Account',
+     :foreign_key => :stream_channel_id
+
+    validates_presence_of :owner
+
+    def refresh_content force=false
+      super force do |new_videos|
+        owner.api.video_mentions_in_feed.each do |m|
+          m.videos.each do |v|
+            v.populate do |video|
+              new_videos << video
+              push video, m.published_at
+            end
+          end
+        end
+      end
+    end
+  end
+end
