bfb265747c115fc8a98b8eecc6cc480c1ea3a978
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-03 01:37:26 -0700

    Use full info hash with Youtube.

--- models/youtube_api.rb
+++ models/youtube_api.rb
@@ -4,7 +4,7 @@ module Aji
     @@client ||= YouTubeIt::Client.new {}
 
     def author_info author_id
-      DataGrabber.new author_id
+      DataGrabber.new(author_id).build_hash
     end
 
     def video_info youtube_id
@@ -59,33 +59,127 @@ module Aji
         @feed_url =
           "http://gdata.youtube.com/feeds/api/users/#{youtube_uid}?alt=json&v=2"
         @data = get_data_from_youtube
+        @hash = build_hash
       end
 
-      def description
+      def uid
+        @data.fetch('id', {}).fetch('$t', "").split(':').last || ""
+      end
+
+      def published
+        time_string = @data.fetch('published', {}).fetch('$t', nil)
+        if time_string then DateTime.parse(time_string).to_time else nil end
+      end
+
+      def updated
+        time_string = @data.fetch('updated', {}).fetch('$t', nil)
+        t = if time_string then DateTime.parse(time_string) else nil end
+        #t.to_time # Why the shit does this not work?
+        t.instance_eval { Time.utc(year, mon, mday, hour, min, sec).getlocal }
+      end
+
+      def category
+        @data.fetch('category', {})[1].fetch('term', '*** undefined ***')
+      end
+
+      def title
+        @data.fetch('title', {}).fetch('$t', "")
+      end
+
+      def about_me
         @data.fetch('yt$aboutMe',{}).fetch('$t', "")
       end
 
-      def profile_uri
-        link = @data.fetch('link',[]).find do |link_hash|
-          link_hash['rel'] == 'alternate'
-        end
-        if link then link['href'] else "" end
+      def profile
+        find_link 'alternate'
       end
 
-      def thumbnail_uri
+      def homepage
+        find_link 'related'
+      end
+
+      def featured_video_id
+        link = find_link 'http://gdata.youtube.com/schemas/2007#featured-video'
+        match = link.match(Link::YOUTUBE_ID_REGEXP)
+        match && match[1] || nil
+      end
+
+      def first_name
+        @data.fetch('yt$firstName', {}).fetch('$t', "")
+      end
+
+      def last_name
+        @data.fetch('yt$lastName', {}).fetch('$t', "")
+      end
+
+      def hobbies
+        @data.fetch('yt$hobbies', {}).fetch('$t', "")
+      end
+
+      def location
+        @data.fetch('yt$location', {}).fetch('$t', "")
+      end
+
+      def occupation
+        @data.fetch('yt$occupation', {}).fetch('$t', "")
+      end
+
+      def school
+        @data.fetch('yt$school', {}).fetch('$t', "")
+      end
+
+      def subscriber_count
+        @data.fetch('yt$statistics', {}).fetch('subscriberCount', "").to_i
+      end
+
+      def thumbnail
         @data.fetch("media$thumbnail", {}).fetch('url', "")
-      end
-
-      def realname
-        first_name = @data.fetch('yt$firstName',{}).fetch('$t', "")
-        last_name = @data.fetch('yt$lastName',{}).fetch('$t', "")
-        [first_name, last_name].join(' ').strip
       end
 
       def get_data_from_youtube
         response = Faraday.get(@feed_url)
         return {} unless response.status == 200
         MultiJson.decode(response.body)['entry']
+      end
+
+      def username
+        @data.fetch('yt$username', {}).fetch('$t', {})
+      end
+
+      def total_upload_views
+        @data.fetch('yt$statistics', {}).fetch('totalUploadViews', "").to_i
+      end
+
+      def find_link link_type
+        link = @data.fetch('link', []).find do |link_hash|
+          link_hash['rel'] == link_type
+        end
+
+        if link then link['href'] else "" end
+      end
+
+      def build_hash
+        {
+          'uid' => uid,
+          'published' => published,
+          'updated' => updated,
+          'category' => category,
+          'title' => title,
+          'profile' => profile,
+          'homepage' => homepage,
+          'featured_video_id' => featured_video_id,
+          'about_me' => about_me,
+          'first_name' => first_name,
+          'last_name' => last_name,
+          'hobbies' => hobbies,
+          'location' => location,
+          'occupation' => occupation,
+          'school' => school,
+          'subscriber_count' => subscriber_count,
+          'thumbnail' => thumbnail,
+          'username' => username,
+          'total_upload_views' => total_upload_views
+        }
       end
     end
   end
