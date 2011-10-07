1d0ef93abd42346fb24da6d4e78be6eb838f066d
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-07 00:15:22 -0400

    Add API Tracking to Twitter.

--- models/twitter_api.rb
+++ models/twitter_api.rb
@@ -10,6 +10,7 @@ module Aji
     end
 
     def video_mentions_in_feed
+      tracker.hit!
       tweets_with_videos = filter_links @client.home_timeline(:count => 200,
        :include_entities => true)
       tweets_with_videos.map{|t| Parsers::Tweet.parse t }.reject do |mention|
@@ -19,16 +20,34 @@ module Aji
       end
     end
 
+    def video_mentions_i_post
+      tracker.hit!
+      tweets_with_videos = filter_links @client.user_timeline(:count => 200,
+        :include_entities => true)
+      tweets_with_videos.map{|t| Parsers::Tweet.parse t }.reject do |mention|
+        processor = MentionProcessor.new mention
+        processor.perform
+        processor.failed? || processor.no_videos?
+      end
+    end
+
     def valid_uid? uid
+      tracker.hit!
       @client.user? uid.to_i
     end
 
     def valid_username? username
+      tracker.hit!
       @client.user? username
     end
 
     def user_info uid
+      tracker.hit!
       @client.user uid.to_i
+    end
+
+    def tracker
+      @@tracker ||= APITracker.new self.class.to_s, 100, 3600, Aji.redis
     end
 
     private
