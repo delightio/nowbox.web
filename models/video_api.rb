a42cfb1034d9461ade4ad5d959a7ab939035e6b1
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-13 18:21:55 -0700

    VideoAPI delegates calls to specified APIs.

--- models/video_api.rb
+++ models/video_api.rb
@@ -1,16 +1,20 @@
 module Aji
-  module VideoAPI
+  class VideoAPI
     Error = Class.new StandardError
 
-    def self.new source
-      source_apis[source].new
+    def initialize sources=VideoAPI.source_apis.keys
+      @apis = sources.map{ |s| VideoAPI.source_apis[s] }
+    end
+
+    def method_missing symbol, *args, &block
+      @apis.map{ |api| api.send(symbol, *args, &block) }.flatten
     end
 
     def self.source_apis
-      {
+      @@source_apis ||= {
         :youtube => YoutubeAPI,
         #:vimeo => VimeoAPI
-      }
+      }.freeze
     end
   end
 end
---
+++ spec/models/video_api_spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+module Aji
+  describe Aji::VideoAPI do
+    describe ".source_apis" do
+      it "uses symbols to point to classes" do
+        VideoAPI.source_apis.each do |symbol, api_class|
+          symbol.should be_kind_of(Symbol)
+          api_class.should be_kind_of(Class)
+        end
+      end
+    end
+
+    describe "#method_missing" do
+      it "delegates to specified sources" do
+        VideoAPI.source_apis.values.each do |source_api|
+          source_api.should_receive(:send).with(:method_name, :arg1, :arg2)
+        end
+
+        subject.method_name :arg1, :arg2
+      end
+
+      it "returns an array containing the results from each source" do
+        VideoAPI.source_apis.values.each do |source_api|
+          source_api.should_receive(:send).with(:meth, :arg1).and_return(
+            [:return_value])
+        end
+
+        subject.meth(:arg1).should == Array.new(VideoAPI.source_apis.count,
+          :return_value)
+
+      end
+
+    end
+  end
+end
