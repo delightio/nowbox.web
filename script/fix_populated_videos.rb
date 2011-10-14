03ac8e982fc2bca9a0dad6d441af4cc72557a4a2
Thomas Pun <tpun@ottomator.com>
2011-10-14 13:18:02 -0700

    forgot to return new offset

--- script/fix_populated_videos.rb
+++ script/fix_populated_videos.rb
@@ -1,14 +1,17 @@
 require_relative '../aji'
 
 def fix_nil_authors count, offset=0
-  Aji.log "Fixing #{count} populated videos with nil authors..."
+  Aji.log "Fixing #{count} populated videos with nil authors (offset: #{offset})..."
   done = 0
+  new_offset = offset
   Aji::Video.find_each(:offset=>0) do |v|
     break if done > count
     if v.has_nil_author?
       Resque.enqueue Aji::Queues::FixPopulatedVideo, v.id
       puts "Video[#{v.id}] enqueued"
       done += 1
+      new_offset = v.id
     end
   end
+  new_offset
 end
