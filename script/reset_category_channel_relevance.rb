2fc76b7f8c2a744fce99f99dc15599880de0eed0
Thomas Pun <tpun@ottomator.com>
2011-10-15 02:09:25 -0700

    updated script [#521]

--- script/reset_category_channel_relevance.rb
+++ script/reset_category_channel_relevance.rb
@@ -7,19 +7,28 @@ module Aji
   end
   puts
 
+  non_yt = []
+  incomplete = []
   [Channel::Account].each do |channel_class|
+    c=0
     Aji.log "Updating from #{channel_class.count} #{channel_class} channels..."
     channel_class.find_each do |ch|
-      Aji.log "  Channel[#{ch.id}], #{ch.title}, #{ch.content_video_id_count}, #{ch.relevance} (#{ch.subscriber_count})"
-
+      print " #{c.to_s.rjust(4)}  Channel[#{ch.id}], #{ch.title},"
+      puts " #{ch.content_video_id_count}, #{ch.relevance} (#{ch.subscriber_count})"
+      c += 1
       if ch.accounts.any? {|a| a.class != Account::Youtube }
-        Aji.log "  ** non youtube acccounts. Skipping..."
+        Aji.log "    ** non youtube acccounts. Skipping..."
+        non_yt << ch.id
         next
       end
 
       all_refreshed = ch.accounts.all? {|a| a.refreshed? }
-      Aji.log "  * Channel[#{ch.id}] does not have all info needed. *" if !all_refreshed
-      ch.update_relevance_in_categories if all_refreshed
+      if all_refreshed
+          ch.update_relevance_in_categories
+      else
+        Aji.log "    *  Channel[#{ch.id}] does not have all info needed. *" if !all_refreshed
+        incomplete << ch.id
+      end
     end
     puts
   end
@@ -28,4 +37,7 @@ module Aji
     Aji.log "Cateogry[#{cat.id}], #{cat.raw_title}, has #{cat.channel_ids.count} channels, " +
       "featured: #{cat.featured_channels.count}, #{cat.featured_channels.map(&:title).join(" | ")}"
   end
+
+  puts "non_yt = #{non_yt}"
+  puts "incomplete = #{incomplete}"
 end
