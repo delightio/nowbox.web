3a1d41c6f54f047bc6c4ab5f3563a4697684a7f0
Thomas Pun <tpun@ottomator.com>
2011-10-20 13:24:47 -0700

    add set_featured_categories

--- script/set_featured.rb
+++ script/set_featured.rb
@@ -8,34 +8,41 @@ require_relative '../aji'
 
 module Aji
 
-  default_regions =  [
-    { :region => Region.en,
-      :channel_titles => %w( NowComedy NowNews NowPopular freddiew ) },
-    { :region => Region.ko,
-      :channel_titles => %w(SMTOWN jypentertainment YGEntertainment sment wondergirls 2pm 2am missA 2NE1 OfficialSe7en) }]
+  def set_featured_channels
+    default_regions =  [
+      { :region => Region.en,
+        :channel_titles => %w( NowComedy NowNews NowPopular freddiew ) },
+      { :region => Region.ko,
+        :channel_titles => %w(SMTOWN jypentertainment YGEntertainment sment wondergirls 2pm 2am missA 2NE1 OfficialSe7en) }]
 
-  default_regions.each do | h |
-    region = h[:region]
-    titles = h[:channel_titles]
+    default_regions.each do | h |
+      region = h[:region]
+      titles = h[:channel_titles]
 
-    puts "Processing #{region.inspect}..."
-    titles.each do |title|
-      channel = Channel.find_by_title title
-      if channel.nil?
-        # Go out and find create new channel
-        q = title
-        account = Account::Youtube.new :uid => q
-        # Search db again just in case the account wasn't indexed for
-        # other reasons, e.g., not enough content videos
-        if account.existing?
-          account = Account::Youtube.find_or_create_by_lower_uid q
-          channel = account.to_channel
+      puts "Processing #{region.inspect}..."
+      titles.each do |title|
+        channel = Channel.find_by_title title
+        if channel.nil?
+          # Go out and find create new channel
+          q = title
+          account = Account::Youtube.new :uid => q
+          # Search db again just in case the account wasn't indexed for
+          # other reasons, e.g., not enough content videos
+          if account.existing?
+            account = Account::Youtube.find_or_create_by_lower_uid q
+            channel = account.to_channel
+          end
         end
+        puts "  Adding #{title}. (Channel[#{channel.id}])..."
+        region.feature_channel channel unless channel.nil?
+        sleep 1
       end
-      puts "  Adding #{title}. (Channel[#{channel.id}])..."
-      region.feature_channel channel unless channel.nil?
-      sleep 1
     end
+  end
+
+  def set_featured_categories
+    featured_by_title = %w(Comedy Entertainment News Games Sports Education Tech Autos Trailers Music Travel Howto )
+    Aji::Category.set_featured featured_by_title
   end
 
 end
