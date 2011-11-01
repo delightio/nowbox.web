de597a3cfd0005023ec48c3ff18397b086a17748
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-11-01 13:29:06 -0700

    Put more information in error message to client.

--- controllers/auth_controller.rb
+++ controllers/auth_controller.rb
@@ -84,7 +84,7 @@ module Aji
 
       rescue => e
         Aji.log :WARN, "#{e.class}: #{e.message}"
-        MultiJson.encode :error => 'Unable to authenticate'
+        MultiJson.encode :error => 'Unable to authenticate', :message => e.message
       end
     end
 
