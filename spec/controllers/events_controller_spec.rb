fdbfcce1ee752d8832b18fe1d0fbed993d3f0692
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-11-02 16:03:17 -0700

    Add test for message as reason in /1/events.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,38 +1,60 @@
 require File.expand_path("../../spec_helper", __FILE__)
 
-module Aji
-  resource = "events"
-  resource_uri = "/#{API.version.first}/#{resource}"
+resource = "events"
+resource_uri = "/#{Aji::API.version.first}/#{resource}"
 
-  describe API do
-    describe "resource: #{resource}" do
-      before(:each) do
-        @user = Factory :user
-        @channel = Factory :youtube_channel_with_videos
-        @video = @channel.content_videos.sample
-      end
-      
-      describe "post #{resource_uri}/" do
-        it "creates event object" do
-          [:video_actions, :channel_actions].each do |actions|
-            params = { :user_id => @user.id, :channel_id => @channel.id }
-            if actions == :video_actions
-              params.merge!(:video_id => @video.id,
-                :video_start => 0, :video_elapsed=>rand(10))
-            end
-            Event.send(actions).each do |action|
-              params.merge! :action => action
-              expect { post("#{resource_uri}/", params) }.to change { Event.count }.by(1)
+include Aji
+
+describe Aji::API do
+  describe "resource: #{resource}" do
+
+    before(:each) do
+      @user = Factory :user
+      @channel = Factory :youtube_channel_with_videos
+      @video = @channel.content_videos.sample
+    end
+
+    describe "POST #{resource_uri}/" do
+      it "creates event object" do
+        [:video_actions, :channel_actions].each do |actions|
+          params = { :user_id => @user.id, :channel_id => @channel.id }
+          if actions == :video_actions
+            params.merge!(:video_id => @video.id,
+                          :video_start => 0, :video_elapsed=>rand(10))
+          end
+          Event.send(actions).each do |action|
+            params.merge! :action => action
+            expect { post("#{resource_uri}/", params) }.to(
+              change { Event.count }.by(1))
               last_response.status.should == 201
-            end
           end
         end
+      end
 
-        it "returns 400 if missing parameters" do
-          post "#{resource_uri}/"
-          last_response.status.should == 400
+      it "returns 400 if missing parameters" do
+        post "#{resource_uri}/"
+        last_response.status.should == 400
+      end
+
+      describe "sharing" do
+        let(:bob) { Factory :user }
+        let(:video) { Factory :video }
+        let(:channel) { Factory :channel }
+        let(:message) { "ZOMG I LOVE THIS VIDEO!!!One" }
+
+        it "takes a message parameter and makes it the reason" do
+          post "#{resource_uri}", :user_id => bob.id, :channel_id => channel.id,
+            :video_id => video.id, :message => message,
+            :action => :share
+          last_response.body.ai
+
+          Event.find(MultiJson.decode(last_response.body)['id']).reason.
+            should == message
+
+          Share.last.message.should == message
         end
       end
     end
   end
 end
+
