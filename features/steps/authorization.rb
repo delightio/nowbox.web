21f0021b2876ecc234c5f9b33c13951274a7f78a
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-11-04 16:37:43 -0700

    Add acceptance test for youtube login.

--- features/steps/authorization.rb
+++ features/steps/authorization.rb
@@ -9,21 +9,37 @@ class Authorization < Spinach::FeatureSteps
     OmniAuth.config.mock_auth[:twitter] = TWITTER_HASH
   end
 
-  @vcr
+  Given  'this youtube account has never been authorized before' do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:you_tube] = YOUTUBE_HASH
+  end
+
   When 'the twitter callback url is triggered with a user_id' do
     VCR.use_cassette "twitter/home_feed" do
       @user = Aji::User.create
       get "auth/twitter/callback?user_id=#{@user.id}"
-
-      @response = last_response
     end
   end
 
+  When 'the youtube callback url is triggered with a user_id' do
+    @user = Aji::User.create
+    get "auth/you_tube/callback?user_id=#{@user.id}"
+  end
+
   Then 'the status code should be 200' do
-    @response.status.should == 200
+    last_response.status.should == 200
   end
 
   And 'there should be no errors' do
-    @response.body.should_not =~ /error/i
+    last_response.body.should_not =~ /error/i
+  end
+
+  And 'the account should be linked with the user\'s identity' do
+    @user.identity.accounts.map(&:uid).include?('nuclearsandwich').
+      should == true
+  end
+
+  And 'a synchronization job should be enqueued' do
+    fail 'not yet implemented'
   end
 end
