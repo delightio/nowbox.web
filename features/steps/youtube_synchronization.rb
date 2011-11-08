039289903273d61cef8b0ff355abf3e7135b7c64
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-11-08 07:42:33 -0800

    Implement feature step definitions.

--- features/steps/youtube_synchronization.rb
+++ features/steps/youtube_synchronization.rb
@@ -1,55 +1,78 @@
 class YoutubeSynchronization < Spinach::FeatureSteps
+  include Aji
+
   feature 'Youtube Synchronization'
+
   Given 'a user authorized with youtube' do
-    raise 'step not implemented'
+    @user = User.create
+    @account = Account::Youtube.from_auth_hash YOUTUBE_HASH
+    Authorization.new(@account, @user.identity).grant!
   end
 
   When 'favoriting a video' do
-    raise 'step not implemented'
+    @video = Video.create! source: :youtube, external_id: 'zxmObqXYgI8<D-'
+    @video.populate
+
+    @user.favorite_video @video, Time.now
   end
 
   Then 'that video should be a yotube favorite' do
-    raise 'step not implemented'
+    @account.api.favorite_videos.include?(@video).should == true
   end
 
   When 'unfavoriting a video' do
-    raise 'step not implemented'
+    @video = Video.create! source: :youtube, external_id: 'zxmObqXYgI8<D-'
+    @video.populate
+    @user.favorite_video @video, Time.now
+    @user.unfavorite_video @video
   end
 
   Then 'that video should not be a yotube favorite' do
-    raise 'step not implemented'
+    @account.api.favorite_videos.include?(@video).should == false
   end
 
   When 'enqueueing a video' do
-    raise 'step not implemented'
+    @video = Video.create! source: :youtube, external_id: 'zxmObqXYgI8<D-'
+    @video.populate
+    @user.enqueue_video @video, Time.now
   end
 
   Then 'that video should be in the watch later playlist on youtube' do
-    raise 'step not implemented'
+    @account.api.watch_later_videos.include?(@video).should == true
   end
 
   When 'dequeueing a video' do
-    raise 'step not implemented'
+    @video = Video.create! source: :youtube, external_id: 'zxmObqXYgI8<D-'
+    @video.populate
+    @user.enqueue_video @video, Time.now
+    @user.dequeue_video @video
   end
 
   Then 'that video should not be in the watch later playlist on youtube' do
-    raise 'step not implemented'
+    @account.api.watch_later_videos.include?(@video).should == false
   end
 
   When 'subscribing to a channel' do
-    raise 'step not implemented'
+    @channel = Channel::Account.create!(
+     accounts: [Account::Youtube.create(uid: "freddiew")])
+
+     @user.subscribe @channel
   end
 
   Then 'that channel should be subscribed on youtube' do
-    raise 'step not implemented'
+    @account.subscriptions.include?(@channel).should == true
   end
 
   When 'unsubscribing from a channel' do
-    raise 'step not implemented'
+    @channel = Channel::Account.create!(
+     accounts: [Account::Youtube.create(uid: "freddiew")])
+
+     @user.subscribe @channel
+     @user.unsubscribe @channel
   end
 
   Then 'that channel should not be subscribed on youtube' do
-    raise 'step not implemented'
+    @account.subscriptions.include?(@channel).should == false
   end
 
   When 'a synchronization occurs' do
