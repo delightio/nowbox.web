7dc1b61ceb6ebd715a1b60cab9ce68581669f0f1
Thomas Pun <tpun@ottomator.com>
2011-11-11 14:33:44 -0800

    use a more generic name for debug queue; added kill_chris

--- queues/kill_chris.rb
+++ queues/kill_chris.rb
@@ -5,22 +5,22 @@ module Aji
     end
   end
 
-  class Queues::KillBill
+  class Queues::KillChris
     @queue = :debug
 
     def self.perform
-      bills_tw_uid = '274048697'
-      bills_fb_uid = '100002238372309'
-      bills_yt_uid = 'testnb1'
+      tw_uid = '408090392' # ChrisNowbox
+      fb_uid = '100002238372309'
+      yt_uid = 'DoctorSbaitso'
 
-      [ Account::Twitter.find_by_uid(bills_tw_uid),
-        Account::Facebook.find_by_uid(bills_fb_uid) ].each do |bill|
+      [ Account::Twitter.find_by_uid(tw_uid),
+        Account::Facebook.find_by_uid(fb_uid) ].each do |bill|
         bill.stream_channel.destroy unless bill.nil? or bill.stream_channel.nil?
       end
 
-      yt = Account::Youtube.find_by_uid bills_yt_uid
+      yt = Account::Youtube.find_by_uid yt_uid
       unless yt.nil?
-        Aji.log "Destroying Account::Youtube[#{bills_yt_uid}]..."
+        Aji.log "Destroying Account::Youtube[#{yt_uid}]..."
 
         Aji.log "Removing Account::Youtube[#{yt.id}]"
         user = yt.user
@@ -28,8 +28,8 @@ module Aji
 
         user.unlink yt
         yt.destroy
-        if Account::Youtube.find_by_uid(bills_yt_uid).nil?
-          Aji.log "Destroyed: #{bills_yt_uid}"
+        if Account::Youtube.find_by_uid(yt_uid).nil?
+          Aji.log "Destroyed: #{yt_uid}"
         else
           Aji.log "Failed to destroy"
         end
