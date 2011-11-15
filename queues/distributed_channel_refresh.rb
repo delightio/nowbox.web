5ba4e992e82c7fc5fb852e7f83fbe2b58750c553
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-11-15 11:59:50 -0800

    Add distributed queue to resque schedule.

--- queues/distributed_channel_refresh.rb
+++ queues/distributed_channel_refresh.rb
@@ -5,8 +5,11 @@ module Aji
     @queue = :refresh_channel
 
     def self.perform
-      Channel.refreshable_types do |channel_type|
+      Channel.refreshable_types.each do |channel_type|
+        next if channel_type.count == 0
+
         interval = 24.hours / channel_type.count + 1
+        puts interval
 
         channel_type.select(:id).each_with_index do |channel, index|
           channel.background_refresh_content interval * index
