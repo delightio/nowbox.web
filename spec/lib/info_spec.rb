3b367006bec0b65f271a724fa6a2021a92d9bab7
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-11-18 11:38:55 -0800

    Forgot to commit updated spec.

--- spec/lib/info_spec.rb
+++ spec/lib/info_spec.rb
@@ -4,10 +4,10 @@ describe Aji::Info do
   describe ".for_device" do
     it "returns a hash of device information" do
       Aji::Info.for_device("ipad").should == {
-        :current_version => "1.0.14b10",
-        :minimum_version => "1.0.12b1",
+        :current_version => "1.0.18",
+        :minimum_version => "1.0.18",
         :link => { :rel => "latest",
-                   :url => "http://itunes.com/apps/googlesearch?" }
+                   :url => "http://tflig.ht/tb9sfs" }
       }
     end
   end
