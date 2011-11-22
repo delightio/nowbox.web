26aff4e552918c0036b1eb55fe034d30cf76a716
Thomas Pun <tpun@ottomator.com>
2011-11-22 15:33:28 -0800

    updated spec for change in #format

--- spec/lib/mixins/formatters_spec.rb
+++ spec/lib/mixins/formatters_spec.rb
@@ -8,20 +8,22 @@ describe Aji::Mixins::Formatters do
 
   describe Aji::Mixins::Formatters::Twitter do
     subject { Class.new { include Aji::Mixins::Formatters::Twitter }.new }
+    let(:long_share) { stub :message => long_message, :link => link_text }
+    let(:short_share) { stub :message => short_message, :link => link_text }
 
     it "shortens messages to less than 140 characters" do
-      subject.format(long_message, link_text).length.should be < 140
-      subject.format(short_message, link_text).length.should be < 140
+      subject.format(long_share).length.should be < 140
+      subject.format(short_share).length.should be < 140
     end
 
     it "includes the link text no matter the message length" do
-      subject.format(long_message, link_text).should include link_text
-      subject.format(short_message, link_text).should include link_text
+      subject.format(long_share).should include link_text
+      subject.format(short_share).should include link_text
     end
 
     it "includes the twitter coda" do
-      subject.format(long_message, link_text).should include coda
-      subject.format(short_message, link_text).should include coda
+      subject.format(long_share).should include coda
+      subject.format(short_share).should include coda
     end
   end
 
