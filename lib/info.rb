5d1a9bd899c401f8c9b0bb8fdc67b84c6bdbf0aa
Thomas Pun <tpun@ottomator.com>
2011-11-23 00:37:07 -0800

    update min. version and download link

--- lib/info.rb
+++ lib/info.rb
@@ -4,10 +4,10 @@ module Aji
       case device
       when "ipad"
         {
-          :current_version => "1.0.18",
-          :minimum_version => "1.0.18",
+          :current_version => "1.0.21",
+          :minimum_version => "1.0.21",
           :link => { :rel => "latest",
-                     :url => "http://tflig.ht/tb9sfs" }
+                     :url => "http://tflig.ht/rWsoP2" }
         }
       else
         raise Aji::Error, "Unknown device type #{device}"
