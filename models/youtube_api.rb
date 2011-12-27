9baf81b36e03bf57107bd6dc1a3937aa08964012
Thomas Pun <tpun@ottomator.com>
2011-12-26 20:56:56 -0800

    relax api tracker

--- models/youtube_api.rb
+++ models/youtube_api.rb
@@ -234,7 +234,7 @@ module Aji
     def tracker
       @tracker ||= if @token and @secret
                      APITracker.new "#{self.class.to_s}:auth", Aji.redis,
-                       cooldown: 20.minutes, hits_per_session: 10000,
+                       cooldown: 10.minutes, hits_per_session: 100000,
                        method_limits: { post: 0.0 }
                    else
                      APITracker.new "#{self.class.to_s}:global", Aji.redis,
