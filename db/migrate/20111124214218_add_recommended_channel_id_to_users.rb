ff8afd356fd3cb315ccb0b678aa57510a8a233de
Thomas Pun <thomas@nowbox.com>
2012-01-30 00:58:26 -0800

    better and more verbal migration [#700]

--- db/migrate/20111124214218_add_recommended_channel_id_to_users.rb
+++ db/migrate/20111124214218_add_recommended_channel_id_to_users.rb
@@ -2,12 +2,16 @@ class AddRecommendedChannelIdToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :recommended_channel_id, :integer
     puts "Creating recommended channel for #{Aji::User.count} users..."
-    Aji::User.find_each do |u|
+    count = 0
+    Aji::User.find_each(:conditions => "recommended_channel_id IS NULL") do |u|
       if u.recommended_channel.nil?
         recommended = Aji::Channel::Recommended.create
         u.update_attribute :recommended_channel_id, recommended.id
+        count += 1
+        puts "#{count} users updated" if count % 1000 == 0
       end
     end
+    puts "Final: #{count} users updated "
     puts "Applying non null constriant..."
     change_column :users, :recommended_channel_id, :integer, :null => false
   end
