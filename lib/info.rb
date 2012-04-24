0c48dcfd7b8caecdd0e700cf80961ad9f96ca830
Thomas Pun <thomas@nowbox.com>
2012-04-24 09:06:01 +0800

    fix version at 1.3

--- lib/info.rb
+++ lib/info.rb
@@ -4,8 +4,8 @@ module Aji
       case device
       when "ipad"
         {
-          :current_version => "1.4",
-          :minimum_version => "1.4",
+          :current_version => "1.3",
+          :minimum_version => "1.3",
           :link => { :rel => "latest",
                      :url => "http://itunes.apple.com/app/nowbox/id464416202?mt=8&uo=4" },
           :links => [{ :rel => "latest",
