d529bbe0ab16766bbca94ae758fdf67b6f4df92e
Thomas Pun <thomas@nowbox.com>
2012-07-01 14:19:51 -0700

    do not blacklist anything

--- lib/mixins/blacklisting.rb
+++ lib/mixins/blacklisting.rb
@@ -5,7 +5,9 @@ module Aji
       # Require a 'blacklisted_at' DateTime database column
 
       def blacklist
-        update_attribute(:blacklisted_at, Time.now) unless blacklisted?
+        # Do not blacklist any video or account.
+        # It will eventually blacklist everything and make Nowbox unusable.
+        # update_attribute(:blacklisted_at, Time.now) unless blacklisted?
       end
 
       def blacklisted?
